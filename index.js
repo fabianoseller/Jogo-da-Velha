9c4ea5a2dc9f9247f4dc2d0b964d5c62a17f0118
Leandro-Santos-Santos <santosleandro1805@gmail.com>
2024-02-25 21:10:29 -0300

    Iniciando o projeto Jogo da velha

---
+++ index.js
@@ -0,0 +1,15 @@
+var jogador1 = 'X';
+var jogador2 = 'o';
+
+var matriz = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+if(jogador1 == 'x'){
+    jogador2 = 'o'; 
+}else{
+    jogador1 = 'x'
+}
+
