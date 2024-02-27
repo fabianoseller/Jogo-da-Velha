603d73f6e00761b5de117bdb0277737b961c6e56
Fabiano Antunes <42979563+fabianoseller@users.noreply.github.com>
2024-02-27 09:41:40 -0300

    Correção...

--- script.js
+++ script.js
@@ -1,50 +1,54 @@
-const currentPlayerElement = document.querySelector(".currentPlayer");
-const gameButtons = document.querySelectorAll(".game button");
-
+const currentPlayer = document.querySelector(".currentPlayer");
+let selected;
 let player = "X";
-let selected = [];
 
-const winningPositions = [
-  [1, 2, 3], [4, 5, 6], [7, 8, 9], // Linhas
-  [1, 4, 7], [2, 5, 8], [3, 6, 9], // Colunas
-  [1, 5, 9], [3, 5, 7]              // Diagonais
+const posicao = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
 ];
 
 function init() {
-  selected = [];
-  currentPlayerElement.textContent = `JOGADOR DA VEZ: ${player}`;
+  selected = new Array(9).fill(null);
+  currentPlayer.textContent = `JOGADOR DA VEZ: ${player}`;
 
-  gameButtons.forEach(button => {
-    button.textContent = "";
-    button.addEventListener("click", newMove);
+  document.querySelectorAll(".game button").forEach((item) => {
+    item.textContent = "";
+    item.addEventListener("click", newMove);
   });
 }
 
 function newMove(e) {
-  const index = parseInt(e.target.getAttribute("data-i"));
+  const index = parseInt(e.target.dataset.i);
   e.target.textContent = player;
   e.target.removeEventListener("click", newMove);
   selected[index] = player;
 
-  check();
+  setTimeout(check, 100);
   
   player = player === "X" ? "O" : "X";
-  currentPlayerElement.textContent = `JOGADOR DA VEZ: ${player}`;
+  currentPlayer.textContent = `Jogador 01: ${player}`;
 }
 
 function check() {
   const playerLastMove = player === "X" ? "O" : "X";
-
-  for (const pos of winningPositions) {
-    if (pos.every(item => selected[item] === playerLastMove)) {
+  
+  for (const pos of posicao
+) {
+    if (pos.every((item) => selected[item - 1] === playerLastMove)) {
       alert(`O JOGADOR '${playerLastMove}' GANHOU!`);
       init();
       return;
     }
   }
 
-  if (selected.filter(item => item).length === 9) {
-    alert("DEU EMPATE!");
+  if (selected.every((item) => item)) {
+    alert("Ichhh Empatou!");
     init();
     return;
   }
